2f9fce3e8ac7f0fc6668b48bd73c15d5994b898b
Gert Wijns <gert.wijns@gmail.com>
2014-09-08 23:24:47 +0200

    Fix: When you open and close Task details the overview also closes

--- src/main/java/be/shad/tl/ui/form/control/TimeLoggerFormControl.java
+++ src/main/java/be/shad/tl/ui/form/control/TimeLoggerFormControl.java
@@ -1,5 +1,7 @@
 package be.shad.tl.ui.form.control;
 
+import javafx.application.Platform;
+import javafx.beans.property.SimpleIntegerProperty;
 import javafx.fxml.FXML;
 import javafx.scene.control.Accordion;
 import javafx.scene.control.TitledPane;
@@ -12,5 +14,14 @@ public class TimeLoggerFormControl extends AbstractFormControl {
     @Override
     protected void initalizeControl() {
         accordion.setExpandedPane(entriesOverviewPane);
+        SimpleIntegerProperty expandedCount = new SimpleIntegerProperty(1);
+        for(TitledPane pane: accordion.getPanes()) {
+            pane.expandedProperty().addListener((prop, o, n) -> {
+                expandedCount.set(expandedCount.get() + (n ? 1: -1));
+                if (expandedCount.get() == 0) {
+                    Platform.runLater(() -> accordion.setExpandedPane(entriesOverviewPane));
+                }
+            });
+        }
     }
 }
